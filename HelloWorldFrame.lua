007d31b54fb4ad9da822af400f914af6526742bf
fraggerfox <santhosh.raju@gmail.com>
2014-02-06 17:20:28 +0530

    First commit of "WoW UI Designer" Generated Frame

---
+++ HelloWorldFrame.lua
@@ -0,0 +1,8 @@
+﻿--
+-- An example Call back for an onClick
+--
+function HelloWorldFrameEvent()
+    label = getglobal("HelloWorldFrameForm".."Label1".."Label");
+    label:SetText("Why did you click me?");
+--     print("hello");
+end
